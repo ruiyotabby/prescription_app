90ca8de7d5d1c16ec7ca2b472ee04f11d4fe9da4
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-18 15:17:22 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -11,12 +12,89 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 100,
-        width: 350,
-        decoration: BoxDecoration(
-          color: Colors.blue[900],
-          borderRadius: BorderRadius.circular(30),
+      body: Center(
+        child: Container(
+          color: Color.fromARGB(255, 250, 218, 218),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'DateField',
+                style: TextStyle(
+                  color: Colors.blue[800],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 500,
+                width: 450,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 4, 38, 90),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              'To take',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 250, 218, 218),
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 250, top: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.add_circle,
+                              color: Color.fromARGB(255, 250, 218, 218),
+                              size: 30,
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 20, horizontal: 8),
+                                child: Builder(builder: ((context) {
+                                  return ListView.builder(
+                                      itemCount: 3,
+                                      itemBuilder: ((context, index) {
+                                        return Card(
+                                          color: Colors.white,
+                                          child: Slidable(child: ListTile()),
+                                        );
+                                      }));
+                                })),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
