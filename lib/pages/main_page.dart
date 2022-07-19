004fe62860bd840c8ddca381711677a636a16896
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-19 14:05:33 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -13,101 +13,101 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          color: Color.fromARGB(255, 250, 218, 218),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                'DateField',
-                style: TextStyle(
-                  color: Colors.blue[800],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 500,
-                width: 450,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 4, 38, 90),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'DateField',
+                  style: TextStyle(
+                    color: Colors.blue[800],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
                   ),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    // crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Text(
-                              'To take',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 250, 218, 218),
-                                fontSize: 25,
-                                fontWeight: FontWeight.w500,
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 500,
+                  width: 450,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 4, 38, 90),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text(
+                                'To take',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 250, 218, 218),
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 240),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Icon(
-                                  Icons.add_circle,
-                                  color: Color.fromARGB(255, 250, 218, 218),
-                                  size: 30,
-                                  
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Card(
-                        color: Colors.white,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 100,
-                              width: 100,
-                              child: Slidable(
-                                key: ValueKey(0),
-                                endActionPane: ActionPane(
-                                  motion: DrawerMotion(),
-                                  children: [
-                                    SlidableAction(
-                                        icon: Icons.check,
-                                        label: 'Done',
-                                        foregroundColor: Colors.white,
-                                        backgroundColor: Colors.red,
-                                        onPressed: ((context) {}))
-                                  ],
-                                ),
-                                child: ListTile(),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 240),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Icon(
+                                    Icons.add_circle,
+                                    color: Color.fromARGB(255, 250, 218, 218),
+                                    size: 30,
+                                  ),
+                                ],
                               ),
                             ),
                           ],
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 20),
+                        Card(
+                          color: Colors.white,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 100,
+                                width: 100,
+                                child: Slidable(
+                                  key: ValueKey(0),
+                                  endActionPane: ActionPane(
+                                    motion: DrawerMotion(),
+                                    children: [
+                                      SlidableAction(
+                                          icon: Icons.check,
+                                          label: 'Done',
+                                          foregroundColor: Colors.white,
+                                          backgroundColor: Colors.red,
+                                          onPressed: ((context) {}))
+                                    ],
+                                  ),
+                                  child: ListTile(),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
