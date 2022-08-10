ded88e693ca635930d733a235ae81e2ca91d8030
Ruiyot Abby <ruiyotabby@gmail.com>
2022-08-10 15:58:58 +0300

    m
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:prescription_app/pages/add.dart';
 import 'package:prescription_app/pages/home.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: _prescriptionApp,
       home: const HomePage(),
     ),
   );
+}
+
+final ThemeData _prescriptionApp = _buildApp();
+
+ThemeData _buildApp() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+      colorScheme: base.colorScheme.copyWith(
+    primary: const Color.fromARGB(255, 253, 222, 233),
+    secondary: const Color.fromARGB(255, 1, 47, 116),
+  ));
 }
