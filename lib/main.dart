be0930bd8de2a4ded564ecefa3d717fe1e07de06
Ruiyot Abby <ruiyotabby@gmail.com>
2022-08-10 14:15:24 +0300

    m
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:prescription_app/pages/add.dart';
 import 'package:prescription_app/pages/home.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: HomePage(),
+      home: const HomePage(),
     ),
   );
 }
